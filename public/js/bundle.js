e5365add4a72e512fe26d7d2ab9552e8c19d7f53
SHAHRIL MAD SHAH <popiang@hotmail.com>
2022-12-27 11:22:02 +0800

    Update Git

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12189,7 +12189,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51101" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64402" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
